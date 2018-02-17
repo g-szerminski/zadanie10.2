1c262b2dcf6c0aeead70146e17c77ba2acd42173
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-17 08:19:29 +0100

    Add event handler for click me button

--- scripts.js
+++ scripts.js
@@ -6,6 +6,9 @@ var span = $('span');
 	});
 var paragraphs = $('p');
 	paragraphs.each(function(index, element) {
-		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-		$(element).append(button)
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+		$(element).append(button);	
 	});
+$('button').click(function(){
+	alert($(this).attr('data-tmp'));
+});
